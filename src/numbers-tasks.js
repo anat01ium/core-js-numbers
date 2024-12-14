011e5556635e1b3d5fb8f9b1fafc4fca42d9bc11
Anatoly Sizyakin <rx.anat01ium@gmail.com>
2024-12-14 12:18:38 +0300

    feat: add implementation of the getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -254,8 +254,19 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0 || index === 1) return index;
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i < index; i += 1) {
+    const buffer = a;
+    a = b;
+    b += buffer;
+  }
+
+  return a + b;
 }
 
 /**
